565c46016009f402d63f2914c076a0168e4d7eb5
Ricardo Durao <ricardo.durao@pvotal.tech>
2024-05-15 16:00:48 +0100

    chore: use web to support WASM compilation

--- lib/maps_launcher_web.dart
+++ lib/maps_launcher_web.dart
@@ -3,8 +3,7 @@ import 'dart:async';
 // In order to *not* need this ignore, consider extracting the "web" version
 // of your plugin as a separate package, instead of inlining it in the same
 // package as the core of your plugin.
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html show window;
+import 'package:web/web.dart' as web show window;
 
 import 'package:flutter/services.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
@@ -40,7 +39,7 @@ class MapsLauncherWeb {
 
   /// Returns a [String] containing the version of the platform.
   Future<String> getPlatformVersion() {
-    final version = html.window.navigator.userAgent;
+    final version = web.window.navigator.userAgent;
     return Future.value(version);
   }
 }
